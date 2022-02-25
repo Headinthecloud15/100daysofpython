6a40061e08450851a7f75991a4f1a68871205b82
Jenny Dassas <JDassas15@gmail.com>
2022-02-24 19:47:34 -0500

    day 1

---
+++ band name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator Game")
+city = input("what city did you grow up in?\n")
+pet = input("what is your pet's name?\n")
+print("Your band name is  " + city + " " + pet)
