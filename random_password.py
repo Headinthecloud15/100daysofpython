2ae9f7577b01f9c1899219238bedec10f5e17380
Jenny Dassas <JDassas15@gmail.com>
2022-02-28 21:03:08 -0500

    random password

---
+++ random_password.py
@@ -0,0 +1,19 @@
+import random
+lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+upper_letters =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+for i in range(2):
+  ll = lower_letters[random.randint(1, len(lower_letters)-1)] 
+  ul = upper_letters[random.randint(1, len(upper_letters)-1)]
+  n = numbers[random.randint(1, len(numbers)-1)]
+  s = symbols[random.randint(1, len(symbols)-1)]
+  password.append(ll + ul + n + s)
+print(password[0] + password[1])
