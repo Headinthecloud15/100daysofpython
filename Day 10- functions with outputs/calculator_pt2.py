658267706fae7eb815e4cec61e890581741cd532
Jenny Dassas <JDassas15@gmail.com>
2022-03-30 22:44:40 -0400

    finished

--- Day 10- functions with outputs/calculator_pt2.py
+++ Day 10- functions with outputs/calculator_pt2.py
@@ -1,3 +1,6 @@
+from calculator_art import logo
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -20,21 +23,25 @@ operators = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-for i in operators:
-    print (i)
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for i in operators:
+        print (i)
 
-not_end = True
+    not_end = True
 
-while not_end == True:
-    operation_symbol = input("pick an operation ")
-    num2 = int(input("What's the next number?: "))
-    calculation_function = operators[operation_symbol]
-    answer = calculation_function(num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    while not_end == True:
+        operation_symbol = input("pick an operation ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operators[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
-    if choice == "y":
-        num1 = answer
-    else:
-        not_end = False
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+        if choice == "y":
+            num1 = answer
+        else:
+            not_end = False
+            calculator()
+calculator()
