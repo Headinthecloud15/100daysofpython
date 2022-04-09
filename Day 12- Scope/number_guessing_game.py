6c79aeabda97a767422203fc8a1ab7e8b78bfe98
Jenny Dassas <JDassas15@gmail.com>
2022-04-08 23:16:09 -0400

    'game

--- Day 12- Scope/number_guessing_game.py
+++ Day 12- Scope/number_guessing_game.py
@@ -1,33 +1,50 @@
-def hard_level():
-    attempts = 5
+EASY_ATTEMPTS = 10
+HARD_ATTEMPTS = 5
 
-def game():
-    end = False
-    while end == False:
-        
-        guess = int(input("Make a guess "))
-        if guess > number:
-            print ("Too high. Guess again")    
-        elif guess < number:
-            print("Too low. Guess again.")
-        elif guess == number:
-            print("You guessed the magic number")
+def difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard'. ")
+    if level == "easy":
+        return EASY_ATTEMPTS
+    if level == "hard":
+         return HARD_ATTEMPTS
+
+
+def answer(guess, number, attempts):
+    if guess > number:
+        print ("Too high. Guess again")
+        return attempts - 1    
+    elif guess < number:
+        print("Too low. Guess again.")
+        return attempts - 1
+    elif guess == number:
+        print("You guessed the magic number")
         
 
 import random
 from guessing_game_logo import logo
 
-number = random.randint(0,100)
+def game():
+    print("Guess the magic number!")
+    print("I'm thinking of a number between 1 and 100")
+    number = random.randint(0,100)
 
-print(f"the answer is {number}")
-attempts = 10
+    print(f"the answer is {number}")
+    attempts = difficulty()
+    
 
-print("Guess the magic number!")
-print("I'm thinking of a number between 1 and 100")
-level = input("Choose a difficulty. Type 'easy' or 'hard'. ")
+    guess = 0
+    while guess != number:
+        print(f"You have {attempts} attempts left remaining")
+        guess = int(input("Make a guess "))
+        attempts = answer(guess, number, attempts)
+        if attempts == 0:
+            print("You have no more attempts. You lost")
+            return
 
-if level == "easy":
-    game()
-if level == "hard":
-    hard_level()
-    game()
+game()
+
+
+
+
+
+    
