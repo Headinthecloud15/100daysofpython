8b4017197225c9b6f0d11c38b0f26b286070685b
Jenny Dassas <JDassas15@gmail.com>
2022-04-10 23:08:50 -0400

    update

--- Day -14 higher lower/higher_lower_game.py
+++ Day -14 higher lower/higher_lower_game.py
@@ -2,17 +2,34 @@
 # key:value pair being compared is follower_count
 import random 
 from game_data import data
-
+from game_art import logo
+from game_art import vs 
 
 #print(random.choice(list(data)))
-# print random dictionary from list data
+# assign random deictionary from data list to variable
 item_a = random.choice(data)
-print(item_a["name"])
-print(item_a["description"])
-print(item_a["country"])
+
+item_b = random.choice(data)
+
+A = int(item_a['follower_count'])
+B = int(item_b['follower_count'])
+   
+
+
+
+final_score = []
+print (logo)
 
 print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}") 
 
+print(vs) 
+print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}") 
 
+
+choice = input("Who has more followers? Type 'A' or 'B':").upper
+if choice > A or choice > B:
+        print("You're right")
+else:
+        print("Sorry that's wrong")
 
 
