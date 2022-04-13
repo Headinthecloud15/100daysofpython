d82a041aa67064597c18ee8ad6721bd48f7177a4
Jenny Dassas <JDassas15@gmail.com>
2022-04-12 21:44:20 -0400

    update

--- Day -14 higher lower/higher_lower_game.py
+++ Day -14 higher lower/higher_lower_game.py
@@ -5,49 +5,49 @@ from game_data import data
 from game_art import logo
 from game_art import vs 
 
-#print(random.choice(list(data)))
+
 # assign random dictionary from data list to variable
 item_a = random.choice(data)
 item_b = random.choice(data)
 if item_a == item_b:
     item_b = random.choice(data)
 
-
-
-
-
-final_score = []
 print (logo)
 
-print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}") 
-
-print(vs) 
-print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}") 
-
-choice = input("Who has more followers? Type 'A' or 'B':").lower() 
-
-a_followers = item_a['follower_count']
-b_followers = item_b['follower_count']
-
-if a_followers > b_followers:
-    if choice == "a":
-        print("You're right")
-    else:
-        print("Sorry that's wrong")
-elif b_followers > a_followers:
-    if choice == "b":
-        print("You're right")
-    else:
-        print("Sorry that's wrong")
 
 
-#highest = max(A,B)
-#print(highest)
+game_end = False
+while game_end == False:
+    print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}") 
 
-#if choice >= highest:
-    #print("You're right")
-#else:
-    #print("Sorry that's wrong")
+    print(vs) 
+    print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}") 
+
+    final_score = 0
+    choice = input("Who has more followers? Type 'A' or 'B':").lower() 
+
+    a_followers = item_a['follower_count']
+    b_followers = item_b['follower_count']
+
+    
+# checks users choice and prints if they are right or wrong
+    if a_followers > b_followers:
+        if choice == "a":
+            final_score += 1
+            print(f"You're right. Current score {final_score}")
+        else:
+            end = True
+            print(f"Sorry that's wrong. Final score {final_score}")
+    elif b_followers > a_followers:
+        if choice == "b":
+            final_score += 1
+            print(f"You're right. Current score {final_score}")
+        else:
+            end = True
+            print(f"Sorry that's wrong. final score {final_score}")
+
+
+
 
 
 
