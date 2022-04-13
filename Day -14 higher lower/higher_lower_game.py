fa587f26355fc7cfedf6d98e758eb3ea5f1762d2
Jenny Dassas <JDassas15@gmail.com>
2022-04-12 21:06:14 -0400

    update

--- Day -14 higher lower/higher_lower_game.py
+++ Day -14 higher lower/higher_lower_game.py
@@ -6,28 +6,13 @@ from game_art import logo
 from game_art import vs 
 
 #print(random.choice(list(data)))
-# assign random deictionary from data list to variable
+# assign random dictionary from data list to variable
 item_a = random.choice(data)
 item_b = random.choice(data)
+if item_a == item_b:
+    item_b = random.choice(data)
 
 
-
-
-def compare():
-    choice = input("Who has more followers? Type 'A' or 'B':").upper 
-    if choice == "A":
-        A = int(item_a['follower_count'])
-    elif choice == "B":
-        B = int(item_b['follower_count'])
-    highest = max(A,B)
-    if choice >= highest:
-        print("You're right")
-    else:
-        print("Sorry that's wrong")
-    
-
-
-   
 
 
 
@@ -39,7 +24,31 @@ print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['co
 print(vs) 
 print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}") 
 
+choice = input("Who has more followers? Type 'A' or 'B':").lower() 
 
-compare()
+a_followers = item_a['follower_count']
+b_followers = item_b['follower_count']
+
+if a_followers > b_followers:
+    if choice == "a":
+        print("You're right")
+    else:
+        print("Sorry that's wrong")
+elif b_followers > a_followers:
+    if choice == "b":
+        print("You're right")
+    else:
+        print("Sorry that's wrong")
+
+
+#highest = max(A,B)
+#print(highest)
+
+#if choice >= highest:
+    #print("You're right")
+#else:
+    #print("Sorry that's wrong")
+
+
 
 
