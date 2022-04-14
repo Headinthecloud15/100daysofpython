0033346242c0f31e88b554bfbae72cbef2e6427c
Jenny Dassas <JDassas15@gmail.com>
2022-04-13 23:29:08 -0400

    finished

--- Day -14 higher lower/higher_lower_game.py
+++ Day -14 higher lower/higher_lower_game.py
@@ -1,54 +1,65 @@
 # Variable data is a list of dictionaries.
 # key:value pair being compared is follower_count
+def clear():
+  print("\n" * 50)
+
+
 import random 
 from game_data import data
 from game_art import logo
 from game_art import vs 
 
 
-# assign random dictionary from data list to variable
-item_a = random.choice(data)
-item_b = random.choice(data)
-if item_a == item_b:
-    item_b = random.choice(data)
+# function that returns random account from data list
+def random_account():
+    return random.choice(data)
 
-print (logo)
+# function that returns data from random account and formats it
+def account_format(account):
+    name = account['name']
+    description = account['description']
+    country = account['country']
+    return f" {name}, a {description}, from {country}"
 
-
-
-game_end = False
-while game_end == False:
-    print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}") 
-
-    print(vs) 
-    print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}") 
-
-    final_score = 0
-    choice = input("Who has more followers? Type 'A' or 'B':").lower() 
-
-    a_followers = item_a['follower_count']
-    b_followers = item_b['follower_count']
-
-    
-# checks users choice and prints if they are right or wrong
+#function that checks user's guess
+def check_answer(guess, a_followers, b_followers):
     if a_followers > b_followers:
-        if choice == "a":
-            final_score += 1
-            print(f"You're right. Current score {final_score}")
-        else:
-            end = True
-            print(f"Sorry that's wrong. Final score {final_score}")
-    elif b_followers > a_followers:
-        if choice == "b":
-            final_score += 1
-            print(f"You're right. Current score {final_score}")
-        else:
-            end = True
-            print(f"Sorry that's wrong. final score {final_score}")
+        return guess == "a"
+    else:
+        return guess == "b"
 
+def game():
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = random_account()
+  account_b = random_account()
 
+  while game_should_continue:
+    account_a = account_b
+    account_b = random_account()
 
+    while account_a == account_b:
+      account_b = random_account()
 
+    print(f"Compare A: {account_format(account_a)}.")
+    print(vs)
+    print(f"Against B: {account_format(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
 
 
