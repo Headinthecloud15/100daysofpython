c13488aecee384c3fd6463204c9fe1b8c4d20330
Jenny Dassas <JDassas15@gmail.com>
2022-04-15 19:42:12 -0400

    first function

--- Day-15 coffee machine/coffee_machine.py
+++ Day-15 coffee machine/coffee_machine.py
@@ -2,10 +2,25 @@ from coffee_data import MENU
 from coffee_data import resources
 
 report = resources
-espresso = MENU["espresso"]
-latte = MENU["latte"]
-cappuccino = MENU["cappuccino"]
 
-#choice = input("What would you like? 'espresso', 'latte', or 'cappuccino'")
 
-print(MENU["espresso"]["ingredients"])
+
+
+
+# function that will calculate ingredients and subtracts from total resources
+
+def coffee_flavor(choice):
+    espresso = MENU["espresso"]["ingredients"]
+    latte = MENU["latte"]["ingredients"]
+    cappuccino = MENU["cappuccino"]["ingredients"]
+    if choice == 'espresso':
+        print(espresso)
+    elif choice == 'latte':
+        print(latte)
+    elif choice == 'cappuccino':
+        print(cappuccino)
+    
+
+
+choice = input("What would you like? 'espresso', 'latte', or 'cappuccino' ")
+coffee_flavor(choice)
