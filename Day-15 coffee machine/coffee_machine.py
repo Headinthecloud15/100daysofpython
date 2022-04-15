bb54b02fa68bd1510bb184c762188397adf83ef7
Jenny Dassas <JDassas15@gmail.com>
2022-04-14 21:08:48 -0400

    first draft

---
+++ Day-15 coffee machine/coffee_machine.py
@@ -0,0 +1,11 @@
+from coffee_data import MENU
+from coffee_data import resources
+
+report = resources
+espresso = MENU["espresso"]
+latte = MENU["latte"]
+cappuccino = MENU["cappuccino"]
+
+#choice = input("What would you like? 'espresso', 'latte', or 'cappuccino'")
+
+print(MENU["espresso"]["ingredients"])
