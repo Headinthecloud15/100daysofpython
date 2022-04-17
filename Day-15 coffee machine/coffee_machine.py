cb669a95dbdf32fdacb0c925fd55f65f45d028f7
Jenny Dassas <JDassas15@gmail.com>
2022-04-16 23:39:40 -0400

    update

--- Day-15 coffee machine/coffee_machine.py
+++ Day-15 coffee machine/coffee_machine.py
@@ -1,14 +1,16 @@
 from coffee_data import MENU
 from coffee_data import resources
 
-report = resources
+
 espresso = MENU["espresso"]["ingredients"]
 latte = MENU["latte"]["ingredients"]
 cappuccino = MENU["cappuccino"]["ingredients"]
-
-# function that will take input and return ingredients
+espresso_price = 1.50
+latte_price = 2.50
+cappuccino_price = 3.00
 
 def coffee_flavor(choice):
+    """function that will take input and return ingredients"""
     if choice == 'espresso':
         return espresso
     elif choice == 'latte':
@@ -16,8 +18,43 @@ def coffee_flavor(choice):
     elif choice == 'cappuccino':
         return cappuccino
     
+    
+#  calculate total amount to pay
+def total_amount(choice):
+    if choice == 'espresso':
+       return espresso_price
+    elif choice == 'latte':
+        return espresso_price
+    elif choice == 'cappuccino':
+        return espresso_price
+
+
 
 choice = input("What would you like? 'espresso', 'latte', or 'cappuccino' ")
+if choice == 'report':
+        print(resources)
+
+
+print(f"You're total is {total_amount(choice)} Please insert coins ")
+
 coffee_ingredients = coffee_flavor(choice)
-print(resources - coffee_ingredients)
+
+# function that will take the input of coffee_ingredients and subtract it from the total resources
+
+# function that will check if transaction is successful
+
+quarters = input("How many quarters? ")
+
+
+
+# Calculate the monetary value of the coins
+quarter = 0.25 * int(quarters)
+dime = 0.10
+nickel = 0.05
+penny = 0.01
+total = quarter 
+print (total)
+# function that will check if resources are sufficient
+
+
 
