ff879fb9335f7f802638b73e1580510bb92c1731
Jenny Dassas <JDassas15@gmail.com>
2022-04-17 21:38:32 -0400

    update

--- Day-15 coffee machine/coffee_machine.py
+++ Day-15 coffee machine/coffee_machine.py
@@ -10,7 +10,7 @@ latte_price = 2.50
 cappuccino_price = 3.00
 
 # dictionary that stores the monetary value of the coins
-money ={
+monetary_value ={
     "quarter" : 0.25, 
     "dime" : 0.10,
    "nickel" : 0.05,
@@ -26,10 +26,10 @@ def coffee_flavor(choice):
         return latte
     elif choice == 'cappuccino':
         return cappuccino
-    
-    
-#  calculate total amount to pay
+     
+
 def total_amount(choice):
+    """function that will return the amount to pay"""
     if choice == 'espresso':
        return espresso_price
     elif choice == 'latte':
@@ -37,21 +37,38 @@ def total_amount(choice):
     elif choice == 'cappuccino':
         return espresso_price
 
-def money_inserted():
-    quarters = int(input("How many quarters? ")) * money["quarter"]
-    dimes = int(input("How many dimes? ")) * money["dime"]
-    nickels = int(input("How many nickels? ")) * money["nickel"]
-    pennies = int(input ("How many pennies? ")) * money ["penny"]
-    total = "{:.2f}".format(quarters + dimes + nickels + pennies)
-    print(total)
-
 choice = input("What would you like? 'espresso', 'latte', or 'cappuccino' ")
 if choice == 'report':
         print(resources)
-
 total_price = "{:.2f}".format(total_amount(choice))
 print(f"You're total is {total_price} Please insert coins ")
+
+def money_inserted():
+    """function that will take the input of the amount of coins inserted and calculate the total"""
+    quarters = int(input("How many quarters? ")) * monetary_value["quarter"]
+    dimes = int(input("How many dimes? ")) * monetary_value["dime"]
+    nickels = int(input("How many nickels? ")) * monetary_value["nickel"]
+    pennies = int(input ("How many pennies? ")) * monetary_value ["penny"]
+    total = ("{:.2f}".format(quarters + dimes + nickels + pennies))
+    if total < total_price:
+        print("Sorry that's not enough money. Money refunded.")
+    elif total > total_price:
+        change = "{:.2f}".format( float(total) - float(total_price))
+        print(f"Here is {change} dollars in change")
+    elif total == total_price:
+        resources["money"] = total_price
+        print(resources)
+
+
 money_inserted()
+
+
+
+
+
+
+    
+
 #coffee_ingredients = coffee_flavor(choice)
 
 # function that will take the input of coffee_ingredients and subtract it from the total resources
