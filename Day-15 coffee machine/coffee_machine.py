46de93b489750165e6ee7266b7c292a6b9d2b418
Jenny Dassas <JDassas15@gmail.com>
2022-04-17 20:58:55 -0400

    update

--- Day-15 coffee machine/coffee_machine.py
+++ Day-15 coffee machine/coffee_machine.py
@@ -9,6 +9,15 @@ espresso_price = 1.50
 latte_price = 2.50
 cappuccino_price = 3.00
 
+# dictionary that stores the monetary value of the coins
+money ={
+    "quarter" : 0.25, 
+    "dime" : 0.10,
+   "nickel" : 0.05,
+    "penny" : 0.01,
+}
+
+
 def coffee_flavor(choice):
     """function that will take input and return ingredients"""
     if choice == 'espresso':
@@ -28,32 +37,33 @@ def total_amount(choice):
     elif choice == 'cappuccino':
         return espresso_price
 
-
+def money_inserted():
+    quarters = int(input("How many quarters? ")) * money["quarter"]
+    dimes = int(input("How many dimes? ")) * money["dime"]
+    nickels = int(input("How many nickels? ")) * money["nickel"]
+    pennies = int(input ("How many pennies? ")) * money ["penny"]
+    total = "{:.2f}".format(quarters + dimes + nickels + pennies)
+    print(total)
 
 choice = input("What would you like? 'espresso', 'latte', or 'cappuccino' ")
 if choice == 'report':
         print(resources)
 
-
-print(f"You're total is {total_amount(choice)} Please insert coins ")
-
-coffee_ingredients = coffee_flavor(choice)
+total_price = "{:.2f}".format(total_amount(choice))
+print(f"You're total is {total_price} Please insert coins ")
+money_inserted()
+#coffee_ingredients = coffee_flavor(choice)
 
 # function that will take the input of coffee_ingredients and subtract it from the total resources
 
 # function that will check if transaction is successful
 
-quarters = input("How many quarters? ")
 
 
 
-# Calculate the monetary value of the coins
-quarter = 0.25 * int(quarters)
-dime = 0.10
-nickel = 0.05
-penny = 0.01
-total = quarter 
-print (total)
+
+
+
 # function that will check if resources are sufficient
 
 
